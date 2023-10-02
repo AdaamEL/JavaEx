93798c42f7f9a1685e1b3a4cd608353a9152e70a
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2023-10-02 10:15:49 +0200

    Add files via upload

---
+++ trois.java
@@ -0,0 +1,8 @@
+public class trois {
+    public static void main(String[] args) {
+        String name = "Halil";
+        Integer age = 27;
+        String job = "développeur";
+        System.out.println("je m'appelle "+ name + ", j'ai "+ age+ " ans et je suis "+ job+".");
+    }
+}
