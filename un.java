00f090c7b56e8575f6136c79da2f1fed45f8c708
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2023-10-02 10:04:42 +0200

    Add files via upload

---
+++ un.java
@@ -0,0 +1,8 @@
+public class un{  
+    public static void main(String[] args) {
+        String name = "Alex";
+        Integer age = 25;
+        String conc = name + age; 
+        System.out.println(conc);
+    }
+}
