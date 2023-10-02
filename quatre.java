7e90c5cb0775f3a6e9982a190f7cafe1d03fb239
AdaamEL <91120983+AdaamEL@users.noreply.github.com>
2023-10-02 11:17:21 +0200

    Add files via upload

---
+++ quatre.java
@@ -0,0 +1,11 @@
+public class quatre {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        Integer i = 0;
+
+        while(i<str.length()){
+            System.out.print(str.charAt(i) + " ");
+            i++;
+        }
+    }
+}
